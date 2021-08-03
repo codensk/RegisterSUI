c850fcf0948a3adb4239c14041882768390e8b7a
Sergey Vorobev <rusbear28@yandex.ru>
2021-08-03 09:09:44 +0700

    Initial Commit

---
+++ RegisterSUI/RegisterSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RegisterSUIApp.swift
+//  RegisterSUI
+//
+//  Created by SERGEY VOROBEV on 03.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct RegisterSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
